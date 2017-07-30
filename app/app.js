35e3745f6ab8967974671b27ce1b19fb6ab4f07b
Dian Gunawan <digoens@gmail.com>
2017-07-30 17:21:33 +0700

    heroku missing bower package

--- app/app.js
+++ app/app.js
@@ -10,3 +10,8 @@ app.config(function($routeProvider) {
     })
     .otherwise( { redirectTo: '/' } );
 });
+
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/app'));
+app.listen(process.env.PORT || 3000);
